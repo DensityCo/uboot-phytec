c18f0a8e51f414debf3df0eb27a2e05ac24fa08a
Matt McKee <mmckee@phytec.com>
2018-11-28 16:29:55 -0800

    AM57SW-655 board: phytec: am57xx_phycore_rdk: add Micron MT41K128M16JT x2 support to EMIF config
    Signed-off-by: Matt McKee <mmckee@phytec.com>

--- board/phytec/am57xx_phycore_rdk/emif_config.h
+++ board/phytec/am57xx_phycore_rdk/emif_config.h
@@ -10,6 +10,12 @@
 #ifndef _EMIF_CONFIG_PCM057_H_
 #define _EMIF_CONFIG_PCM057_H_
 
+const struct dmm_lisa_map_regs am57xx_phycore_rdk_512Mx1_lisa_regs = {
+	.dmm_lisa_map_3 = 0xFF020100,
+	.dmm_lisa_map_2 = 0x80500100,
+	.is_ma_present  = 0x1
+};
+
 const struct dmm_lisa_map_regs am57xx_phycore_rdk_1Gx1_lisa_regs = {
 	.dmm_lisa_map_3 = 0xFF020100,
 	.dmm_lisa_map_2 = 0x80600100,
@@ -33,6 +39,32 @@ const struct dmm_lisa_map_regs am572x_phycore_rdk_1Gx2_lisa_regs = {
 	.dmm_lisa_map_3 = 0xFF020100,
 	.dmm_lisa_map_2 = 0x80780300,
 	.is_ma_present  = 0x1
+};
+
+/* MT41K128M16JT-125 memory timings */
+const struct emif_regs am57xx_phycore_rdk_emif_532mhz_128M16_regs = {
+	.sdram_config_init	= 0x61851ab2,
+	.sdram_config		= 0x61851ab2,
+	.sdram_config2		= 0x08000000,
+	.ref_ctrl		= 0x000040f1,
+	.ref_ctrl_final		= 0x00001035,
+	.sdram_tim1		= 0xeeef2673,
+	.sdram_tim2		= 0x305c7fda,
+	.sdram_tim3		= 0x409f8558,
+	.read_idle_ctrl		= 0x00090000,
+	.zq_config		= 0x5007190b,
+	.temp_alert_config	= 0x00000000,
+	.emif_ddr_phy_ctlr_1_init = 0x0024400b,
+	.emif_ddr_phy_ctlr_1	= 0x0e24400b,
+	.emif_ddr_ext_phy_ctrl_1 = 0x10040100,
+	.emif_ddr_ext_phy_ctrl_2 = 0x00910091,
+	.emif_ddr_ext_phy_ctrl_3 = 0x00950095,
+	.emif_ddr_ext_phy_ctrl_4 = 0x009b009b,
+	.emif_ddr_ext_phy_ctrl_5 = 0x009e009e,
+	.emif_rd_wr_lvl_rmp_win	= 0x00000000,
+	.emif_rd_wr_lvl_rmp_ctl	= 0x80000000,
+	.emif_rd_wr_lvl_ctl	= 0x00000000,
+	.emif_rd_wr_exec_thresh	= 0x00000305
 };
 
 /* MT41K256M16TW-107 memory timings */
