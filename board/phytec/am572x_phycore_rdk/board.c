bec5eb495fb556a8b4318c2d963543f426b6defd
Casey Kelso <casey@caseykelso.com>
2022-03-31 11:51:53 -0700

    .

--- board/phytec/am572x_phycore_rdk/board.c
+++ board/phytec/am572x_phycore_rdk/board.c
@@ -288,11 +288,11 @@ err:
 #if !defined(CONFIG_SPL_BUILD) && defined(CONFIG_GENERIC_MMC)
 int board_mmc_init(bd_t *bis)
 {
-// disable emmc init for development purposes
-//	omap_mmc_init(0, 0, 0, -1, -1)
+// sdcard init for development purposes
+	omap_mmc_init(0, 0, 0, -1, -1)
 
-	// sdcard init
-	omap_mmc_init(1, 0, 0, -1, -1);
+	// emmc init
+//	omap_mmc_init(1, 0, 0, -1, -1);
 
 	return 0;
 }
